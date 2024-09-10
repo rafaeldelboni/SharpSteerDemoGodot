f16773ea6a3b53355edc506a4dc2019e52233172
Rafael Delboni <rafadelboni@gmail.com>
2024-09-10 17:46:40 -0300

    feat: add textures and small readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+# SharpSteerDemoGodot
+
+Sample of SharpSteer with Godot 
+
+## Licenses
+ - [Prototype Textures](https://www.kenney.nl/assets/prototype-textures) by Kenney is licensed under [Creative Commons CC0](https://creativecommons.org/publicdomain/zero/1.0/).  
+All the rest of the project is free and unencumbered software released into the public domain. For more information, please refer to http://unlicense.org
--- scripts/Obstacle.cs
+++ scripts/Obstacle.cs
@@ -10,12 +10,14 @@ public partial class Obstacle : Node3D
 
     public override void _Ready()
     {
+        var material = GD.Load<Material>($"res://assets/material/obstacle_material.tres");
         mesh = GetNode<MeshInstance3D>("MeshInstance3D");
         var cylinder = new CylinderMesh();
         cylinder.Height = 0.1f;
         cylinder.TopRadius = Radius;
         cylinder.BottomRadius = Radius;
         mesh.Mesh = cylinder;
+        mesh.MaterialOverride = material;
         sphericalObstacle = new(Radius, Position.ToNumerics());
     }
 }
